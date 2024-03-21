a90239a2a290073a4b00d9cbe1e78aae1f77e564
임중훈 <imjunghun@imjunghun-ui-MacBookPro.local>
2024-03-21 13:47:30 +0900

    Initial commit

---
+++ src/main/java/com/miniproject/racingcar/RacingcarApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.racingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniproject/racingcar/RacingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.racingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
